1f180d30046756d473d9838705c98fb03513d1b9
git-piyush03 <piyush.as32@gmail.com>
2025-08-09 03:47:27 +0530

    Initial commit

---
+++ The Bengal Bite/src/script.js
@@ -0,0 +1,7 @@
+// JavaScript for mobile menu toggle
+        const mobileMenuButton = document.getElementById('mobile-menu-button');
+        const mobileMenu = document.getElementById('mobile-menu');
+
+        mobileMenuButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
